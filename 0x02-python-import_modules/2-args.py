6a5c445a6c2d8e28901afaaeda250272c18b2a08
NatePillay <nathan94pillay@gmail.com>
2022-12-08 05:34:19 -0800

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    if(args <=  1):
+    if(args <= 1):
         print("{} arguments:".format(args - 1))
     elif (args == 2):
         print("{} argument:".format(args - 1))
