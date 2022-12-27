a9a446cc995851dbbc31b48a1b578cea10436e70
NatePillay <nathan94pillay@gmail.com>
2022-12-27 11:16:23 -0800

    new commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_numbers = map(lambda x: x**2, numbers)
-    print(list(squared_numbers))
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
