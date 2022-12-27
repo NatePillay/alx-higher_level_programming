5e2cdf7fd34c58f1e0cfa168c1ac5da7453a21e3
NatePillay <nathan94pillay@gmail.com>
2022-12-27 13:54:14 -0800

    new commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = dictionary.copy()
-    new_dict = new_dict * 2
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
     return new_dict
