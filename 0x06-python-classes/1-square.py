2c87d909c507d2d8eeb975d554099282d78da754
NatePillay <nathan94pillay@gmail.com>
2023-01-03 01:14:14 -0800

    new touch

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py by Nathan Pillay
+""" Define a square """
+
+class Square:
+    """ Represent a square"""
+    def __init__(self, size):
+        """ Initialise square class
+        args: size as an attribute """
+        self.__size = size
