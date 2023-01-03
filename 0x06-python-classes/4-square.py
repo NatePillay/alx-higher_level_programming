685d1af453139395fedf963711b7c900adf1120f
NatePillay <nathan94pillay@gmail.com>
2023-01-03 03:12:06 -0800

    new ne new new

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#square-3.py by Nathan Pillay
+"""Defining a square"""
+
+
+class Square:
+	""" a class that represents a square"""
+
+	def __init__(self, size=0):
+            """
+            initialising this square class
+            Args:
+            	size: this is an attribute of the square
+            Raises:
+            	TypeError for anything that is not an integer
+            	ValueError for size less then 0
+            """
+
+            if not isinstance(size, int):
+                raise TypeError('size must be an integer')
+            if size < 0:
+                raise ValueError('size must be >= 0')
+
+            self.__size = size
+
+	@property
+	def size(self):
+            """retrieve size of square"""
+            return self.__size
+
+	@size.setter
+	def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError('size must be >= 0')
+
+            self.__size = value
+
+	def area(self):
+            """
+            Calculate the area of a square
+            Return size ** 2
+            """
+
+            return (self.__size ** 2)
