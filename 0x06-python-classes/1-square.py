79fb09f1e3dcc62d2546d24a766bf0725418192d
NatePillay <nathan94pillay@gmail.com>
2023-01-03 01:17:48 -0800

    new touch

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,9 @@
 
 class Square:
     """ Represent a square"""
-    
+
     def __init__(self, size):
         """ Initialise square class
         args: size as an attribute """
-        
+
         self.__size = size
