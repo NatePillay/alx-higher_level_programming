3f66f1d47cbdaa357397b4a7bbb84ed1798bd37e
NatePillay <nathan94pillay@gmail.com>
2023-01-03 20:12:06 -0800

    new commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#task 0 by Nathan Pillay
+""" creating a class called Rectangle """
+
+class Rectangle:
+    """an empty class no attributes"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+2021 to 2023 learning python and only noe seeing eval
