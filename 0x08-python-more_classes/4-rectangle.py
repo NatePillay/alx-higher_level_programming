88d7b433f1de527c6f31a454ed6da2e83b8a8bd1
NatePillay <nathan94pillay@gmail.com>
2023-01-06 05:57:58 -0800

    new commit

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,4 +62,4 @@ class Rectangle:
             return ('\n'.join('#' * seld.width for x in range(self.height)))
         
         def __repr__(self):
-            return ("Rectangle({}, {})".format(self.__width, self.__height))
+            return ("Rectangle({}, {})".format(self.width, self.height))
