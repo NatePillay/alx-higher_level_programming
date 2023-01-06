dd90f1037e3382d5d5ac7b720698bb7d52d9b6d0
NatePillay <nathan94pillay@gmail.com>
2023-01-05 21:21:23 -0800

    keep commiting

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """A class that will define the dimensions of rectangle"""
+
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
         Args:
@@ -25,7 +26,6 @@ class Rectangle:
         def width(self, value):
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
-
             if value < 0:
                 raise ValueError("width must be >= 0")
             self.__width = value
@@ -39,20 +39,19 @@ class Rectangle:
         def height(self, value):
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
-
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
 
         def area(self):
             """Returns the area of a rectangle"""
-            return self.__width * self.__height
+            return (self.__width * self.__height)
 
         def perimeter(self):
             """Returns the perimeter of a rectangle"""
             if self.__width == 0 or self.__height == 0:
-                return 0
-            return 2 * (self.__width + self.__height)
+                return (0)
+            return (2 * (self.__width + self.__height))
 
         def __str__(self) -> str:
             """Returns a shape made with #'s"""
@@ -64,7 +63,7 @@ class Rectangle:
                     rectangle += '#'
                 if i < self.__height - 1:
                     rectangle += '\n'
-            return rectangle
+            return (rectangle)
         
         def __repr__(self):
             return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
