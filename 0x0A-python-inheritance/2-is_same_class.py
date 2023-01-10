50c99f17a6a61d7f8b09534cd6455d6d262097a2
NatePillay <nathan94pillay@gmail.com>
2023-01-09 20:58:12 -0800

    new commit tyre

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Return true if object is an instance of the class, otherwise return false"""
-    return (type(obj) == a_class)
+    return obj.__class__ == a_class
