3e291f679fb85a432079026551bb64122518c649
NatePillay <nathan94pillay@gmail.com>
2023-01-10 12:50:45 -0800

    new new ne

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a base geometry"""
+
+class BaseGeometry:
+        """this class represents a base geometry"""
+
+        def area(self):
+            """method not implemented yet"""
+            raise Exception ("area() is not implemented")
+
+        def integer_validator(self, name, value):
+            """method to validate integer"""
+            if not isinstance(value, int):
+                raise TypeError ("{} must be an integer".format(name))
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
