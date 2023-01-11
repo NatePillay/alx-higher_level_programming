260a7706b413f216bcaae7c987a39436bf09f0fc
NatePillay <nathan94pillay@gmail.com>
2023-01-11 06:17:35 -0800

    new commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that returns a python data structure"""
+
+
+def from_json_string(my_str):
+    """Returnes the json representation of a string object"""
+    return json.loads(my_str)
