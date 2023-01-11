193e17c07b2f546c4fe64eb80a510925e6ff4df0
NatePillay <nathan94pillay@gmail.com>
2023-01-11 13:14:58 -0800

    new try

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""we are inheriting here"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class we are using is a square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
