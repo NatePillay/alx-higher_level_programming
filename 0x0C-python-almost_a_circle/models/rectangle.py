fe4ab7bf2487c6d2113e6f70ca3d4e667cb243d0
NatePillay <nathan94pillay@gmail.com>
2023-01-17 12:50:36 -0800

    new commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """keep it test free"""
 
+from models.base import Base
+
 
 class Rectangle(Base):
+    """ Represents a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
