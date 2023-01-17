d0226e9c22c6fcb27f0844bbec50cb4e0bbf6971
NatePillay <nathan94pillay@gmail.com>
2023-01-17 13:19:42 -0800

    keep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,9 @@ from models.base import Base
 
 class Rectangle(Base):
     """ Represents a rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initializes attributes of the object"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -43,6 +45,7 @@ class Rectangle(Base):
 
         @property
         def x(self):
+            """set"""
             return self.__x
 
         @x.setter
@@ -56,6 +59,7 @@ class Rectangle(Base):
 
         @property
         def y(self):
+            """set"""
             return self.__y
 
         @y.setter
