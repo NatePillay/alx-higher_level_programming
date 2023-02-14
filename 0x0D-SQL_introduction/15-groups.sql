63f6c3216733d6624005f0357f402f44e5ce41ac
NatePillay <nathan94pillay@gmail.com>
2023-02-14 15:15:47 -0800

    new commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- select the number of record with same score and create column for it 
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY score DESC
