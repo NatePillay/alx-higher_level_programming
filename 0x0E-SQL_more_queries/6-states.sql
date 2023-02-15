ad07d5724cf6ee88765157348ea8bf530644c682
NatePillay <nathan94pillay@gmail.com>
2023-02-15 14:07:42 -0800

    new commit

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- create BD and Table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL DEFAULT 1,
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256),
-	UNIQUE (id)
+	PRIMARY KEY (id)
 	);
