8e4ca00164433d322b71dce63875ed15281f2639
NatePillay <nathan94pillay@gmail.com>
2023-03-06 20:59:07 -0800

    new commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 for i in range(1,101):
     result = ""
-    if (i % 3 == 0):
-        result += "Fizz"
-    if (i % 5 == 0):
-        result += "Buzz"
-    print("{}".format(result) or "{}".format(i), end='')
-    print(" ", end='')
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz", end=" ")
+    elif (i % 3 == 0):
+        print("Fizz", end=" ")
+    elif (i % 5 == 0):
+        print("Buzz", end=" ")
+    else:
+        print(i, end= " ")
