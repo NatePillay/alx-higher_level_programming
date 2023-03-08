58044501a759fce903fc971ac0a19a861aae36ad
NatePillay <nathan94pillay@gmail.com>
2023-03-07 21:51:27 -0800

    new commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
         for number in element:
-            print("{:d}".format(number), end="")
-            if(number is not element[len(element)-1]):
-                print(" ", end="")
-        print()
+            if number == element[-1]:
+                print("{:d}".format(number), end="")
+            else:
+                print("{:d}".format(number), end=" ")
+        print('')
