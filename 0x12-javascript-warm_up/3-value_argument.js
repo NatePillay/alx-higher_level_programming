963ab2fe2c91fc0c7e9b3f8370659cce5e6f85a7
NatePillay <nathan94pillay@gmail.com>
2023-03-14 02:33:25 +0000

    new commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
-console.log("No argument"); }
-else if (args.length === 1) {
-console.log(args[0]); }
-else {
-console.log(args);
-}
+console.log(process.argv[2] ? process.argv[2]: 'No argument');
