59b5dd5a7ea4be3fc992642b2a27be5c5c9ed042
NatePillay <nathan94pillay@gmail.com>
2023-03-19 09:13:28 -0700

    new commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,6 +14,6 @@ Base = declarative_base()
 
 class State(Base):
    """ Reps a state for MySQL db"""
-    __tablename__ = 'states'
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
