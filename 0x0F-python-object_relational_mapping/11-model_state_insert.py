203b8bc2c5c776b082d92196738c6bea9f24ae5b
NatePillay <nathan94pillay@gmail.com>
2023-03-20 14:49:56 -0700

    new commit with add and commit sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the
+ database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    new_record = States(state.name = "Louisiana")
+
+    session.add(new_record)
+    session.commit()
+    print(new_record.id)
