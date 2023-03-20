cd04906209838b5dfb526d2e86f5eceb55d151a1
NatePillay <nathan94pillay@gmail.com>
2023-03-20 13:33:42 -0700

    pycodestyle errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    script that lists all states matching user input 
+    script that lists all states matching user input
     from the database hbtn_0e_0_usa
     Usage:
     ./2-my_filter_states.py <mysql username> <mysql password> <database name>
@@ -12,5 +12,6 @@ import MySQLdb
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name LIKE BINARY '{}' ORDER BY `id`".format(argv[4]))
+    cur.execute("SELECT * FROM `states` WHERE name LIKE BINARY '{}' \
+            ORDER BY `id`".format(argv[4]))
     [print(state) for state in cur.fetchall()]
