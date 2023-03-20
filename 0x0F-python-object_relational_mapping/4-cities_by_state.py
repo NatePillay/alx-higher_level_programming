c555935dd0fdae2f9831e477992f062b5b4276cd
NatePillay <nathan94pillay@gmail.com>
2023-03-20 13:39:03 -0700

    fetchall:

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" 
+"""
 Display all values for city in DB hbtn_0e_0_usa
 """
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], port=3306, host="localhost",passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], port=3306, host="localhost", passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("""SELECT cities.id, cities.name, states.name
-                 FROM cities JOIN states ON cities.state_id = states.id
-                 ORDER BY cities.id ASC""")
+    c.execute("""SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            ORDER BY cities.id ASC""")
     [print(city) for city in c.fetchall()]
