e3133bf6658b47691a6b0bb1595f714341c31b1f
NatePillay <nathan94pillay@gmail.com>
2023-03-20 13:43:11 -0700

    {:s}

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,5 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM cities JOIN states ON cities.state_id = states.id WHERE states.name LIKE '{:s}' ORDER BY cities.id ASC".format(argv[4]))
+    c.execute("SELECT * FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name LIKE '{:s}' ORDER BY cities.id ASC".format(argv[4]))
     print(", ".join([city[2] for city in c.fetchall()]))
