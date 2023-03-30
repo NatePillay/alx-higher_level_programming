ce18e02de060f18c46740b3ea8e78309ec79c664
NatePillay <nathan94pillay@gmail.com>
2023-03-30 13:57:28 -0700

    new commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#curl command using post here and variables
+#send a POST req command using post here and variables
 curl -s -X POST -d "email:test@gmail.com&subject=I will always be here for PLD" "$1"
