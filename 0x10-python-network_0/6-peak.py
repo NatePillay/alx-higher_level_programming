1e84f94a58c5112b5e997696b059c6cad150e610
NatePillay <nathan94pillay@gmail.com>
2023-03-30 14:33:02 -0700

    new commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    max  = list_of_integers[0]
-    for i in list_of_integers:
-        if list_of_integers[1] > max:
-            max = list_of_integers[1]
-    return max
+    return max(list_of_integers)
