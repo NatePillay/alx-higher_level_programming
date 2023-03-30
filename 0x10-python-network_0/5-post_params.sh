570f8a16d18664f7c4925239a851a28ea6a4b9c9
NatePillay <nathan94pillay@gmail.com>
2023-03-30 13:56:07 -0700

    forgot my magic -s

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl command using post here and variables
-curl -X POST -d "email:test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email:test@gmail.com&subject=I will always be here for PLD" "$1"
