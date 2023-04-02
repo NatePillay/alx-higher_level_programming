b83f848573d93a5a144e47a909a7eb32eee04141
NatePillay <nathan94pillay@gmail.com>
2023-04-02 04:52:09 -0700

    nrw commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,13 @@
     also handle an error
 """
 from sys import argv
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 from urllib.error import HTTPError
 
 if __name__ == '__main__':
-    url = argv[1]
-    req = Request(url)
     try:
-        with urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
 
     except HTTPError as e:
         print('Error code: {}'.format(e.code))
