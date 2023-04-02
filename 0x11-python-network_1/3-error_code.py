f35f591e55be63bc17ebeb5c04c494a9957aa37a
NatePillay <nathan94pillay@gmail.com>
2023-04-02 04:46:24 -0700

    new commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script takes in URL and display response
+    also handle an error
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+if __name__ == '__main__':
+    url = argv[1]
+    req = Request(url)
+    with urlopen(req) as response:
+        try:
+            print(response.read().decode('utf-8'))
+
+        except HTTPError as e:
+            print('Error code: {}'.format(e.code))
+
+
+
