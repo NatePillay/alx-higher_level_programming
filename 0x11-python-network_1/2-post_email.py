5e534f593bd584906060a8948ab3e4e612232f2b
NatePillay <nathan94pillay@gmail.com>
2023-04-02 04:24:05 -0700

    new commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,9 +10,8 @@ from urllib.request import Request, urlopen
 
 if __name__ == '__main__':
     url = argv[1]
-    email = argv[2]
 
-    data = parse.urlencode(email).encode('ascii')
+    data = parse.urlencode({'email': argv[2]}).encode('ascii')
     req = Request(url, data)
     with urlopen(req) as response:
         print(response.read().decode('utf-8'))
