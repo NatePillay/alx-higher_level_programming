09cbfd68b40ff76a1a50e7dee011aa7544b47c41
NatePillay <nathan94pillay@gmail.com>
2023-04-07 14:26:32 -0700

    new commit|

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#take in URL and email
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+
+    response = requests.post(url, data=data)
+    print(response)
