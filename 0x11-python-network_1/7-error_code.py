173ceb5d98bffddc0072e2ca11b610f2c60520b8
NatePillay <nathan94pillay@gmail.com>
2023-04-09 03:02:01 -0700

    new commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == '__main__':
     url = argv[1]
 
-    r =  requests.get(url)
+    r = requests.get(url)
 
     if r.status_code >= 400:
         print("Error code:", r.status_code)
