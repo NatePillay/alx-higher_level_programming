7eb906b855ab017b8baf140ccd42962460f4ac21
NatePillay <nathan94pillay@gmail.com>
2023-04-09 06:07:24 -0700

    using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""using map function to find the square val of all ints in this matrix"""
-
-
 def square_matrix_map(matrix=[]):
-    squared_matrix = list(map(lambda row: [val ** 2 if isinstance(val, int) \
+    return list(map(lambda row: [val ** 2 if isinstance(val, int) \
             else val for val in row], matrix))
-    return squared_matrix
