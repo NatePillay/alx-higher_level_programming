7ac174ae1620fdd1552d7539f1484872de3fc57f
NatePillay <nathan94pillay@gmail.com>
2023-04-09 06:25:13 -0700

    new commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     keys_to_delete = []
-    for key, val in d.items():
-        if val == value:
+    for key, val in a_dictionary.items():
+        if value == val:
             keys_to_delete.append(key)
 
     for key in keys_to_delete:
-        del d[key]
+        del a_dictionary[key]
 
-    return d
+    return a_dictionary
