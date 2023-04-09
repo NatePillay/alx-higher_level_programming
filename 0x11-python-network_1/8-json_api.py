801f1acd16fec15c9aab6c9b8a83f6882a735946
NatePillay <nathan94pillay@gmail.com>
2023-04-09 03:44:26 -0700

    post method check json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script using request library for web servers"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from requests import post
+
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = " "
+
+    response = post(url, data={"q": q})
+
+    try:
+        res_json = response.json()
+        if res_json == {}:
+            print("No result")
+        else:
+            print(f'[{res_json.get("id"))}] {res_json.get("name")}')
+    except ValueError:
+        print("Not a valid JSON")
