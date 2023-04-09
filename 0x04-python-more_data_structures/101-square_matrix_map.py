3dedf156a71c6a12a7d102239baf068ed6a36fb4
NatePillay <nathan94pillay@gmail.com>
2023-04-09 06:08:14 -0700

    new chance to commit with map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: [val ** 2 if isinstance(val, int) \
-            else val for val in row], matrix))
+    return list(map(lambda row: [val ** 2 if isinstance(val, int) else val for val in row], matrix))
