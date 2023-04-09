13ebcd3775d1b06b9cd0be9e889ff9fb99f17ad9
NatePillay <nathan94pillay@gmail.com>
2023-04-09 03:51:44 -0700

    new commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+from requests import get
+from requests.auth import HTTPBasicAuth
+
+if __name__ == '__main__':
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    res = get("https://api.github.com/user", auth=auth)
+    print(res.json().get('id'))
