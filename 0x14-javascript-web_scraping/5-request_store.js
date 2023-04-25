cbd4faf1d3867d08d9d5983cd373ba742b290fe9
NatePillay <nathan94pillay@gmail.com>
2023-04-25 22:34:22 +0000

    new commit

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const endpoint = process.argv[2];
 const filepath = process.argv[3];
 
-request.get(endpoint, (error) => {
+request.get(endpoint, (error, response, body)=> {
 	if (error) throw error;
 	fs.writeFile(filepath, body, 'utf-8', (error) => {
 		if (error) throw error;
