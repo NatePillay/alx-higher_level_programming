30f75ab9d116c6620497bb2576c57f20fcf5c05e
NatePillay <nathan94pillay@gmail.com>
2023-04-25 21:04:57 +0000

    new commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringWrite = process.argv[3];
 
-fs.writeFile(filePath, stringWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, stringWrite, 'utf-8', (err, data) => {
 	if (err) {
 		console.error(err);
 	} else {
-		console.log('${stringWrite}');
+		console.log(data.toString());
 	}
 });	
