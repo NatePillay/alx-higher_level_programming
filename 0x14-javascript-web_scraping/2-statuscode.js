06301493f6bec87378660f124d70e8065310490e
NatePillay <nathan94pillay@gmail.com>
2023-04-25 21:28:53 +0000

    new commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const request = require('require');
 
 const url = process.argv[2];
-request(endpoint, (err, res, body) => {
+request(url, (err, res, body) => {
 	if (!err) console.log('code:',res.statusCode);
 });
