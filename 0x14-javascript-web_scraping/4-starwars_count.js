04e67c31a3246dd8d1b1b4ea4be1074ec5e643cd
NatePillay <nathan94pillay@gmail.com>
2023-04-25 22:14:21 +0000

    new commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const endpoint = process.argv[2];
+let count  = 0;
+
+request(endpoint, (error, response, body) => {
+	if (error) throw error;
+	
+	JSON.parse(body).results.forEach(result => {
+		result.character.forEach(character => {
+			if (character.endsWith('18/')) count++;
+			});
+		});
+		console.log(count);
+});
