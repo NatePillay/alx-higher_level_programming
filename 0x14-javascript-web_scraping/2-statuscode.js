21d54fc8fb912cf6915f9107e67baaa92cb79b68
NatePillay <nathan94pillay@gmail.com>
2023-04-25 21:29:36 +0000

    nawe

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('require');
 
 const url = process.argv[2];
 request(url, (err, res, body) => {
-	if (!err) console.log('code:',res.statusCode);
+	if (!err) console.log('code:', res.statusCode);
 });
