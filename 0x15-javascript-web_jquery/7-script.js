198d18df4b1c4bcfe188d2706242411e9abc1a59
NatePillay <nathan94pillay@gmail.com>
2023-05-04 08:10:11 +0200

    new commit

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,9 @@
-$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
-    $('#character').text(data.name);
-});
+$(function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://swapi.co/api/people/5/?format=json',
+      success: function (data) {
+        $('DIV#character').text(data.name);
+      }
+    });
+  });
